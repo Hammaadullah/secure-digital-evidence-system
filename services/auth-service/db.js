1ae51e6217c12fe5601a9555372be3d68f8bb383
Mohammed Akeel <choudharyahmed44@gmail.com>
2026-02-13 17:25:38 +0530

    cr

--- services/auth-service/db.js
+++ services/auth-service/db.js
@@ -1,11 +1,12 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  host: "localhost",
-  port: 5432,
-  user: "evidence_user",
-  password: "evidence_pass",
-  database: "evidence_db"
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
 
 module.exports = pool;
+
